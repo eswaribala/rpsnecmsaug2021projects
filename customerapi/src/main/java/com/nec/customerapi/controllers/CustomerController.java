2b47001f34a1e0f8c77afb3448d85860f34a02ea
eswaribala <parameswaribala@gmail.com>
2021-08-31 13:19:42 +0530

    updated

--- customerapi/src/main/java/com/nec/customerapi/controllers/CustomerController.java
+++ customerapi/src/main/java/com/nec/customerapi/controllers/CustomerController.java
@@ -10,17 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nec.customerapi.models.Customer;
 import com.nec.customerapi.services.CustomerService;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
     @Autowired    
 	private CustomerService customerService;
     
-    @PostMapping("/customers")
+    @PostMapping({"/v1.0", "/v1.1"})
+
     public ResponseEntity<?> addCustomer(@RequestBody Customer customer){
     	
     	Customer obj=this.customerService.addCustomer(customer);
@@ -31,17 +34,19 @@ public class CustomerController {
     	
     }
     
-    @GetMapping("/customers")
+    @GetMapping({"/v1.0", "/v1.1"})
     public List<Customer> getCustomers(){
     	return this.customerService.getCustomers();
     }
     
-    @GetMapping("/customers/{customerId}")
+    @GetMapping({"/v1.0/{customerId}", "/v1.1/{customerId}"})
+
     public Customer getCustomerById(@PathVariable("customerId") long customerId) {
     	return this.customerService.getCustomerById(customerId);
     }
     
-    @DeleteMapping("/customers/{customerId}")
+    @DeleteMapping({"/v1.0/{customerId}", "/v1.1/{customerId}"})
+
     public ResponseEntity<?> deleteCustomerById(@PathVariable("customerId") long customerId) {
     	if(this.customerService.deleteCustomerById(customerId))
     		return ResponseEntity.status(HttpStatus.ACCEPTED).body("customer"
--- customerapi/src/main/java/com/nec/customerapi/models/FullName.java
+++ customerapi/src/main/java/com/nec/customerapi/models/FullName.java
@@ -3,7 +3,10 @@ package com.nec.customerapi.models;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import lombok.Data;
+
 @Embeddable
+@Data
 public class FullName {
     @Column(name="First_Name",nullable = false,length = 50) 
 	private String firstName;
