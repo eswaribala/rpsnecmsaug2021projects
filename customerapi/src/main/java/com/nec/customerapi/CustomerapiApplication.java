c0477952ad4dc6bd4d2d53f9c84f7cd7ccb0bf34
eswaribala <parameswaribala@gmail.com>
2021-09-01 10:21:06 +0530

    updated

--- customerapi/src/main/java/com/nec/customerapi/CustomerapiApplication.java
+++ customerapi/src/main/java/com/nec/customerapi/CustomerapiApplication.java
@@ -2,7 +2,11 @@ package com.nec.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * http://localhost:7070/customers/v1.0/pages
+ *http://localhost:7070/customers/v1.0/pages?sort=email,asc
+ *http://localhost:7070/customers/v1.0/pages?page=0&size=2&sort=email,desc
+ */
 @SpringBootApplication
 public class CustomerapiApplication {
 
--- customerapi/src/main/java/com/nec/customerapi/controllers/CustomerController.java
+++ customerapi/src/main/java/com/nec/customerapi/controllers/CustomerController.java
@@ -1,8 +1,16 @@
 package com.nec.customerapi.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +26,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.bohnman.squiggly.Squiggly;
 import com.github.bohnman.squiggly.util.SquigglyUtils;
 import com.nec.customerapi.models.Customer;
+import com.nec.customerapi.repositories.CustomerRepository;
 import com.nec.customerapi.services.CustomerService;
 
 @RestController
@@ -25,6 +34,8 @@ import com.nec.customerapi.services.CustomerService;
 public class CustomerController {
     @Autowired    
 	private CustomerService customerService;
+    @Autowired
+    private CustomerRepository customerRepository;
     
     @PostMapping({"/v1.0", "/v1.1"})
 
@@ -71,5 +82,65 @@ public class CustomerController {
   		return SquigglyUtils.stringify(mapper, customerList);
   		
       }
+  	
+  	
+  	@GetMapping({"/v1.0/pages","/v1.1/pages"})
+    public ResponseEntity<Map<String, Object>> getAllCustomerPage(
+        @RequestParam(required = false) String email,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "3") int size,
+        @RequestParam(defaultValue = "customerId,desc") String[] sort) {
+
+      try {
+        List<Order> orders = new ArrayList<Order>();
+
+        if (sort[0].contains(",")) {
+          // will sort more than 2 fields
+          // sortOrder="field, direction"
+          for (String sortOrder : sort) {
+            String[] _sort = sortOrder.split(",");
+            orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
+          }
+        } else {
+          // sort=[field, direction]
+          orders.add(new Order(getSortDirection(sort[1]), sort[0]));
+        }
+
+        List<Customer> customers = new ArrayList<Customer>();
+        PageRequest pagingSort = PageRequest.of(page, size, Sort.by(orders));
+
+        Page<Customer> pageCustomers;
+        if (email == null)
+          pageCustomers = customerRepository.findAll(pagingSort);
+        else
+          pageCustomers =customerRepository.findByEmailContaining(email, pagingSort);
+
+        customers = pageCustomers.getContent();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("tutorials", customers);
+        response.put("currentPage", pageCustomers.getNumber());
+        response.put("totalItems", pageCustomers.getTotalElements());
+        response.put("totalPages", pageCustomers.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+      } catch (Exception e) {
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
+
+  	
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+          return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+          return Sort.Direction.DESC;
+        }
+
+        return Sort.Direction.ASC;
+      }
+
+  	
+  	   
 
 }
