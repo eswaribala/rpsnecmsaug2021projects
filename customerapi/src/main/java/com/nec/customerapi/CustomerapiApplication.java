1d7a69f5a33efdd82efa2ba57486b39a510ccc5e
eswaribala <parameswaribala@gmail.com>
2021-09-01 11:15:48 +0530

    updated

--- customerapi/src/main/java/com/nec/customerapi/CustomerapiApplication.java
+++ customerapi/src/main/java/com/nec/customerapi/CustomerapiApplication.java
@@ -2,6 +2,8 @@ package com.nec.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 /*
  * http://localhost:7070/customers/v1.0/pages
  *http://localhost:7070/customers/v1.0/pages?sort=email,asc
@@ -13,5 +15,8 @@ public class CustomerapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerapiApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
---
+++ customerapi/src/test/java/com/nec/customerapi/CustomerControllerTest.java
@@ -0,0 +1,66 @@
+package com.nec.customerapi;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.nec.customerapi.models.Customer;
+
+
+
+
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(classes = CustomerapiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles(value = "integration")
+public class CustomerControllerTest {
+	@Rule
+    public WireMockRule wireMockRule = new WireMockRule(9998);
+
+	 @Autowired
+	    private TestRestTemplate testRestTemplate;
+
+
+    @Before
+    public void setUp() {
+        mockRemoteService();
+    }
+    @Test
+    public void testAllCustomers() {
+    	//real test case
+        ResponseEntity<List<Customer>> responseEntity = testRestTemplate.exchange("http://localhost:7070/customers/v1.0", HttpMethod.GET,null,
+                new ParameterizedTypeReference<List<Customer>>(){}
+        );
+        List<Customer> customerList = responseEntity.getBody();
+        System.out.println("CustomerList--->" + customerList.size());
+    }
+
+    private void mockRemoteService() {
+    	//pre configured response
+        stubFor(get(urlEqualTo("/customers/v1.0"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("Tested mock......")
+                ));
+    }
+}
